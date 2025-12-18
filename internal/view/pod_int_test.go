4c9cc3e7e4d11c4e8f3a4a53b709b477455804e4
rlaope <piyrw9754@gmail.com>
2025-12-18 11:52:11 +0900

    test :: pod deletetion select row test

--- internal/view/pod_int_test.go
+++ internal/view/pod_int_test.go
@@ -4,10 +4,16 @@
 package view
 
 import (
+	"context"
 	"strings"
 	"testing"
 
+	"github.com/derailed/k9s/internal"
+	"github.com/derailed/k9s/internal/client"
+	"github.com/derailed/k9s/internal/config/mock"
+	"github.com/derailed/k9s/internal/model1"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 )
 
@@ -79,4 +85,71 @@ func TestComputeShellArgs(t *testing.T) {
 			assert.Equal(t, u.e, strings.Join(args, " "))
 		})
 	}
+}
+
+func TestPodSelectRowByPath(t *testing.T) {
+	po := NewPod(client.PodGVR)
+	ctx := context.WithValue(context.Background(), internal.KeyApp, NewApp(mock.NewMockConfig(t)))
+	require.NoError(t, po.Init(ctx))
+
+	pod, ok := po.(*Pod)
+	require.True(t, ok, "NewPod should return *Pod")
+
+	table := pod.GetTable()
+
+	data := model1.NewTableDataWithRows(
+		client.PodGVR,
+		model1.Header{
+			model1.HeaderColumn{Name: "NAMESPACE"},
+			model1.HeaderColumn{Name: "NAME"},
+		},
+		model1.NewRowEventsWithEvts(
+			model1.RowEvent{
+				Row: model1.Row{
+					ID:     "default/pod1",
+					Fields: model1.Fields{"default", "pod1"},
+				},
+			},
+			model1.RowEvent{
+				Row: model1.Row{
+					ID:     "default/pod2",
+					Fields: model1.Fields{"default", "pod2"},
+				},
+			},
+			model1.RowEvent{
+				Row: model1.Row{
+					ID:     "default/pod3",
+					Fields: model1.Fields{"default", "pod3"},
+				},
+			},
+		),
+	)
+
+	cdata := table.Update(data, false)
+	table.UpdateUI(cdata, data)
+
+	pod.selectRowByPath("default/pod2")
+
+	selectedItem := table.GetSelectedItem()
+	assert.Equal(t, "default/pod2", selectedItem)
+
+	pod.selectRowByPath("default/nonexistent")
+
+	selectedItem = table.GetSelectedItem()
+	assert.Equal(t, "default/pod2", selectedItem)
+}
+
+func TestPodSelectRowByPathWithEmptyTable(t *testing.T) {
+	po := NewPod(client.PodGVR)
+	ctx := context.WithValue(context.Background(), internal.KeyApp, NewApp(mock.NewMockConfig(t)))
+	require.NoError(t, po.Init(ctx))
+
+	pod, ok := po.(*Pod)
+	require.True(t, ok, "NewPod should return *Pod")
+
+	pod.selectRowByPath("default/pod1")
+
+	table := pod.GetTable()
+	selectedItem := table.GetSelectedItem()
+	assert.Empty(t, selectedItem)
 }
